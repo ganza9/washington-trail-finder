b378e29e71213742ada8d69f69c1d4112ef18e5b
Guest User <Guest@epicodus-10.local>
2017-11-06 14:21:04 -0800

    Update app with components and app routing.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { TrailDetailComponent } from './trail-detail/trail-detail.component';
+import { SearchComponent } from './search/search.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    TrailDetailComponent,
+    SearchComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/app.routing.ts
@@ -0,0 +1,27 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+// const appRoutes: Routes = [
+//   {
+//     path: '',
+//     component:
+//   },
+//   {
+//     path: 'map',
+//     component: MapComponent
+//   },
+//   {
+//     path: 'trail-detail',
+//     component: TrailDetailComponent
+//   },
+//   {
+//     path: 'search',
+//     component: SearchComponent
+//   },
+//   {
+//     path: 'result',
+//     component: ResultComponent
+//   }
+// ];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
