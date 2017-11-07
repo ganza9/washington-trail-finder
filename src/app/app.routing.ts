d9dd054451c541eec50dc37e2512a86667a77ecd
richa.shaurbh <richa.shaurabh@gmail.com>
2017-11-07 12:30:29 -0800

    add route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,11 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 //
 // import { TrailDetailComponent } from './trail-detial/trail-detail.component';
 import { MapComponent } from './map/map.component';
+import { SearchComponent } from './search/search.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     component: MapComponent
+  },
+  {
+    path: 'search',
+    component: SearchComponent
   },
 //   {
 //     path: 'trail-detail',
